3e6738e4f43b2ad8d50572b7304dd2eb6a02d415
GiannisKav7 <58071478+GiannisKav7@users.noreply.github.com>
2025-08-04 19:04:47 +0300

    Add PostgreSQL database integration and properties route
    Introduced a database configuration using dotenv and pg, and added a new /properties route to fetch property data from the database. Updated app.js to support environment-based port configuration. Added Express and PostgreSQL documentation links to README.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nodejs
 A minimal Node.js middleware.
+
+**Documentation for Express
+https://expressjs.com/en/starter/static-files.html
+https://blog.logrocket.com/crud-rest-api-node-js-express-postgresql/
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const path = require('path');
+const pool = require('../config/database'); // Adjust the path as necessary
 const router = express.Router();
 
-// Serve the index.html file for the root route
+// POOL STEPS
+// starting async query
+// async query finished
+// starting callback query
+// calling end
+// callback query finished
+// pool has drained
+
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
+
+router.get('/properties', async (req, res) => {
+  try{
+    const result = await pool.query('SELECT * FROM property');
+    res.json(result.rows);
+  }catch (err){
+    console.error('Database error:', err);
+    res.status(500).json({ error: 'Database connection failed' });
+  }
+})
 
 module.exports = router;
