224fbe93ec61d1eca277611367acd16c66fd9d1a
GiannisKav7 <58071478+GiannisKav7@users.noreply.github.com>
2025-08-06 12:55:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // CORS middleware MUST come before routes
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://prop-management-platform.web.app',
   credentials: true
 }));
 
